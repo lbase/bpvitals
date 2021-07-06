1686b2c85e812ac7b09cd188e38288687232a9d7
lbase <lbase@finormile.com>
2021-07-06 15:40:26 -0400

    sqlite

---
+++ showquery.py
@@ -0,0 +1,77 @@
+import sys
+import os
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlQueryModel, QSqlTableModel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableView
+from PyQt5.QtCore import QCoreApplication
+from icecream import ic
+from icecream.icecream import IceCreamDebugger
+from showquerywin import Ui_MainWindow
+from vitals_code import Main as VMain
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.conn_name = "dbshowqry"
+        self.db = QSqlDatabase.addDatabase("QSQLITE", self.conn_name)
+        self.db.setDatabaseName("/home/rfile/python3/bpvitals/vitals.db")
+        ok = self.db.open()        
+        self.model = QSqlTableModel(db=self.db)
+        self.model.setTable("vsigns_bp")
+        self.ui.tb1.setModel(self.model)
+        self.model.setSort(0, Qt.DescendingOrder)
+        # self.model.setFilter()
+        self.model.select()
+        self.ui.btnVform.clicked.connect(self.show_vmain)
+        self.ui.btnRefresh.clicked.connect(self.refreshrecs)
+        self.ui.actionExit.triggered.connect(self.exitFunc)
+        self.ui.btnGraphbp.clicked.connect(self.bpgraph)
+        self.ui.btnDelete.clicked.connect(self.delrows)
+        self.showSbar("Setup Complete")
+    def show_vmain(self):
+        if  'vform' in self.__dict__:
+            self.vform.window.close()
+            self.vform = None
+            self.showSbar("Vitals form closed")    
+            
+        else:
+            self.vform = VMain()
+            self.vform.show()
+            self.showSbar("Vitals form open")
+    def refreshrecs(self):
+        self.model.select()
+        self.ui.tb1.reset()
+    def bpgraph(self):
+        self.showSbar("showing graphs")
+        os.system("/home/rfile/python3/bin/bpstats.py")
+    def showSbar(self, msg):
+        self.ui.statusbar.showMessage(msg)
+    def delrows(self):
+        self.showSbar("remove highlighted rows")
+        l = self.ui.tb1.selectedIndexes()
+        if not len(l): 
+            return
+        else:
+            self.showSbar(str(len(l)/8) + " rows selected")    
+        rows = set([i.row() for i in l])
+        rows = list(rows)
+        rows.sort()
+        first = rows[0]
+        count = len(rows)
+        self.model.removeRows(first, count)
+        self.model.submitAll()
+
+    def exitFunc(self):
+        self.db.close()
+        ic(self.model.lastError().text())
+        self.close()    
+        
+        
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec_()
+
---
+++ vitals_code.py
@@ -0,0 +1,97 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery, QSqlQueryModel
+from PyQt5.QtWidgets import QDataWidgetMapper, QTableView
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QModelIndex, QDateTime, Qt, QVariant
+from icecream import ic
+from addvitals import Ui_Form
+
+import numpy as np
+
+
+class Main(QtWidgets.QWidget, Ui_Form):
+    def __init__(self):
+        super(Main, self).__init__()
+        # build ui
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.conn_name = "vcode"
+        self.db = QSqlDatabase.addDatabase("QSQLITE", self.conn_name)
+        self.db.setDatabaseName("/home/rfile/python3/bpvitals/vitals.db")
+        ok = self.db.open()
+        if ok:
+            self.model = QSqlTableModel(db=self.db)
+            ic(self.model)
+            self.model.setTable("vsigns_bp")
+            self.model.setSort(0, Qt.AscendingOrder)
+            # self.model.setFilter()
+            self.model.select()
+            # self.db.close()   # going to open close in  recinsert
+        else:
+            dbDlg = QMessageBox(self)
+            dbDlg.setWindowTitle("Database error")
+            dbDlg.setText("Database did not connect \n" +
+                          self.db.driverName() + " \n" + self.db.lastError().text())
+            dbDlg.exec()
+            sys.exit()
+
+        sysnp = np.arange(120, 149, 1)
+        for i in sysnp:
+            self.ui.cmbsystolic.addItem(str(i))
+        dianp = np.arange(75, 100, 1)
+        for j in dianp:
+            self.ui.cmbdiastolic.addItem(str(j))
+        pulsenp = np.arange(60, 90, 1)
+        for k in pulsenp:
+            self.ui.cmbheartrate.addItem(str(k))
+        sugarnp = np.arange(75, 140, 1)
+        for l in sugarnp:
+            self.ui.cmbsugar.addItem(str(l))
+        oxyitems = ["96", "97"]
+        self.ui.cmboxy.addItems(oxyitems)
+        now = QDateTime.currentDateTime()
+        self.ui.dateTimeEdit.setDateTime(now)
+
+        self.ui.btnInsert.clicked.connect(self.recinsert)
+        self.ui.btnExit.clicked.connect(self.exitfunc)
+        # self.ui.btnRefresh.clicked.connect(self.setupdb)
+        # self.ui.btnInsert.clicked.connect(self.mapper.submit)
+
+    def recinsert(self):
+        
+        #r.setValue("bpid", myrow)
+        # r.setValue("bpid", "DEFAULT")
+        # r.setValue("bpdate" , QDateTime.currentDateTime())
+        r = self.model.record() 
+        r.setValue("bpdate", self.ui.dateTimeEdit.dateTime())
+        r.setValue("bpsys", self.ui.cmbsystolic.currentText())
+        r.setValue("bpdia", self.ui.cmbdiastolic.currentText())
+        r.setValue("bphr", self.ui.cmbheartrate.currentText())
+        r.setValue("bpsugar", self.ui.cmbsugar.currentText())
+        r.setValue("bpoxy", self.ui.cmboxy.currentText())
+        r.setValue("bpcomment", self.ui.lncomment.toPlainText())
+        self.model.insertRecord(-1, r)
+        self.model.submit()
+        ic(self.model.lastError().text())
+        self.model.select()
+        ic(self.model.submit())
+        # self.db.close()
+
+    def refresh(self):
+        self.model.select()
+
+    def exitfunc(self):
+        self.db.close()
+        self.model.database().close()
+        if self.db.isOpen():
+            self.db.removeDatabase(self.conn_name)
+        ic(self.model.lastError().text())
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec_())
