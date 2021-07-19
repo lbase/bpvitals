ebfc0019fc646682875d3f8676031ca2d6da17ae
lbase <lbase@finormile.com>
2021-07-19 12:17:31 -0400

    import weight and mess with date

--- notes_code.py
+++ notes_code.py
@@ -1,4 +1,3 @@
-
 import datetime
 
 from icecream.icecream import IceCreamDebugger
@@ -12,9 +11,12 @@ import sqlalchemy as dbsql
 from sqlalchemy.orm import sessionmaker
 from icecream import ic
 import sys
+
+
 class Main(QtWidgets.QWidget, Ui_Comment):
     """docstring for Main
 """
+
     def __init__(self, table_name="mynotes"):
         super(Main, self).__init__()
         self.table_name = table_name
@@ -22,21 +24,22 @@ class Main(QtWidgets.QWidget, Ui_Comment):
         self.ui.setupUi(self)
 
         # self.conn_notes = self.eng.connect()  # use this as connection for insert query
-         # ---------------------------------------------------------------------------- #
-         #           make sqlalchemy session and database connection                    #
-         # ---------------------------------------------------------------------------- #
+        # ---------------------------------------------------------------------------- #
+        #           make sqlalchemy session and database connection                    #
+        # ---------------------------------------------------------------------------- #
         self.eng = dbsql.create_engine(
-            "sqlite:////home/rfile/python3/bpvitals/vitals.db")
+            "sqlite:////home/rfile/python3/bpvitals/vitals.db"
+        )
         self.mysession = sessionmaker(bind=self.eng)
         self.mysess = self.mysession()
         self.db = QSqlDatabase.addDatabase("QSQLITE")
         self.db.setDatabaseName("/home/rfile/python3/bpvitals/vitals.db")
         self.ok = self.db.open()
-        ic(self.ok , self.db.driverName())
+        ic(self.ok, self.db.driverName())
 
-         # ---------------------------------------------------------------------------- #
-         #              table name in window title so I can tell what table              #
-         # ---------------------------------------------------------------------------- #
+        # ---------------------------------------------------------------------------- #
+        #              table name in window title so I can tell what table              #
+        # ---------------------------------------------------------------------------- #
         self.setWindowTitle(table_name)
         # current date
         now = QDateTime.currentDateTime()
@@ -56,18 +59,19 @@ class Main(QtWidgets.QWidget, Ui_Comment):
         self.bpid = self.mysess.execute("select max(bpid) as 'bpmax' from vsigns_bp")
         self.spin_bp = self.bpid.fetchone()
         self.ui.spinBpid.setValue(self.spin_bp.bpmax)
-        self.texbx = self.mysess.execute("select fnotes from mynotes where foodid = (select max(foodid) from mynotes)")
+        self.texbx = self.mysess.execute(
+            "select fnotes from mynotes where foodid = (select max(foodid) from mynotes)"
+        )
         self.texbx_txt = self.texbx.fetchone()
         self.ui.textEdit.setText(self.texbx_txt.fnotes)
 
         self.model = QSqlTableModel(db=self.db)
         self.model.setTable("vsigns_bp")
-        self.model.setFilter('bpid = (select max(bpid) from vsigns_bp)')
+        self.model.setFilter("bpid = (select max(bpid) from vsigns_bp)")
         self.model.select()
 
         self.tbl = self.ui.tblPrevRec
         self.tbl.setModel(self.model)
-
 
         ic(self.ok)
         ic(self.model.filter())
@@ -78,25 +82,42 @@ class Main(QtWidgets.QWidget, Ui_Comment):
         # ic(self.db.connectionNames())
         # ic(self.db.tables())
 
-
-
     def update_rec(self):
         self.note = self.ui.textEdit.toPlainText()
         self.id = str(self.ui.spinBpid.value())
-        self.stmt = "update mynotes set fnotes  = " + " '" + self.note + "' " + "where bpid = 681"
+        self.stmt = (
+            "update mynotes set fnotes  = "
+            + " '"
+            + self.note
+            + "' "
+            + "where bpid = 681"
+        )
         ic(self.stmt)
-        self.mysess.execute(self.stmt)
+        self.rec_ins = self.mysess.execute(self.stmt)
         self.mysess.commit()
+        if self.rec_ins:
+            self.ui.lblLastRecord.setText("Record Update " + self.table_name)
 
     def add_rec(self):
-        self.my_table = dbsql.table(self.table_name, dbsql.column("fdate"),\
-            dbsql.column("sugarid"), dbsql.column("bpid"),\
-            dbsql.column("fnotes"))
-        self.ins = self.my_table.insert().values({"fdate": self.ui.dateTimeEdit.dateTime().toString(),\
-            "fnotes": self.ui.textEdit.toPlainText(),\
-                "sugarid": self.ui.spinBsid.value(), "bpid": self.ui.spinBpid.value()})
+        self.my_table = dbsql.table(
+            self.table_name,
+            dbsql.column("fdate"),
+            dbsql.column("sugarid"),
+            dbsql.column("bpid"),
+            dbsql.column("fnotes"),
+        )
+        self.ins = self.my_table.insert().values(
+            {
+                "fdate": self.ui.dateTimeEdit.dateTime().toString(),
+                "fnotes": self.ui.textEdit.toPlainText(),
+                "sugarid": self.ui.spinBsid.value(),
+                "bpid": self.ui.spinBpid.value(),
+            }
+        )
         self.result = self.mysess.execute(self.ins)
         self.mysess.commit()
+        if self.result:
+            self.ui.lblLastRecord.setText("Record Added " + self.table_name)
 
 
 if __name__ == "__main__":
---
+++ weight_lite.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# rfile
+# from file:///home/rfile/python3/notebooks/bpinfo/weight.ipynb
+import logging
+import sys
+import pandas as pd
+import re
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.sqlite import DATETIME
+from icecream import ic
+
+
+def main(filename):
+    try:
+
+        engstr = "sqlite:////home/rfile/python3/bpvitals/vitals.db"
+        eng = create_engine(engstr)
+        myconn = eng.connect()
+        # log
+        logger = logging.getLogger("dev")
+        logger.setLevel(logging.INFO)
+        fileHandler = logging.FileHandler("/home/rfile/python3/bin/log/weight.log")
+        fileHandler.setLevel(logging.INFO)
+        logger.addHandler(fileHandler)
+        formatter = logging.Formatter(
+            "%(asctime)s  %(name)s  %(levelname)s: %(message)s"
+        )
+        fileHandler.setFormatter(formatter)
+        # log
+        # read file
+        wtdata = pd.read_csv(filename)
+        # ic(wtdata)
+        # wtdata.Time = wtdata.rename({"Time": "ftime"})
+        wtdata = change_column_names(wtdata, "Time", "ftime")
+        listtime = wtdata.ftime
+        ic(listtime)
+        # ALTER SEQUENCE fatty_wtid_seq RESTART WITH 10
+        wtdata.columns = wtdata.columns.str.lower()
+        wtdata.columns = wtdata.columns.str.replace(" ", "_")
+        wtdata.columns = wtdata.columns.str.replace("-", "_")
+        # this one working
+        # store time data in var to put it back after replace statement below - easier than iterate through the columns
+        # which had to be done to get the float data
+        # listtime = DATETIME(wtdata.time)
+        # listtime = wtdata.time = pd.to_datetime(wtdata.time)
+
+        wtdata = wtdata.replace(to_replace="\s\D*", value="", regex=True)
+        wtdata.ftime = listtime  # time back in proper format for sql import
+        wtdata[
+            [
+                "weight",
+                "bmi",
+                "body_fat",
+                "fat_free_body_weight",
+                "subcutaneous_fat",
+                "visceral_fat",
+                "body_water",
+                "muscle_mass",
+                "skeletal_muscles",
+                "bone_mass",
+                "protein",
+                "bmr",
+            ]
+        ] = wtdata[
+            [
+                "weight",
+                "bmi",
+                "body_fat",
+                "fat_free_body_weight",
+                "subcutaneous_fat",
+                "visceral_fat",
+                "body_water",
+                "muscle_mass",
+                "skeletal_muscles",
+                "bone_mass",
+                "protein",
+                "bmr",
+            ]
+        ].apply(
+            pd.to_numeric
+        )
+        wtdata = wtdata.sort_values("ftime")
+        wtdata.to_sql("fatty", myconn, if_exists="append", index=False)
+        logger.info("query ran on sqlite : %s ", filename)
+    except Exception as e:
+        print("sorry, an error occurred  ", e)
+        logger.error("sqlite error:  %s", filename)
+
+
+def change_column_names(as_pandas, old_name, new_name):
+    as_pandas.rename(columns={old_name: new_name}, inplace=True)
+    return as_pandas
+
+
+if __name__ == "__main__":
+    filename = "file:///home/rfile/motog3/Bob - Export Data 7-11-2021 ~ 7-17-2021.csv"
+    main(filename)
