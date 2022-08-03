8317f6d543e239a78713a5233727f5254c6dec7e
lbase <lbase@finormile.com>
2022-08-03 16:16:54 -0400

    weight_code.py working using spin boxes to limit entry

--- weight_code.py
+++ weight_code.py
@@ -15,7 +15,7 @@ class Main(QtWidgets.QWidget, Wform):
     def __init__(self):
         super(Main, self).__init__()
         # build ui
-        self.mytable = "fatty"
+        self.mytable = "qfatty"
         self.ui = Wform()
         self.ui.setupUi(self)
         self.conn_name = "wcode"
@@ -47,21 +47,49 @@ class Main(QtWidgets.QWidget, Wform):
         now = QDateTime.currentDateTime()
         self.ui.dateTimeEdit.setDateTime(now)
         self.ui.dateTimeEdit.setDisplayFormat("yyyy-MM-dd HH:mm")
-        self.ui.cmbWeight.setValidator(QtGui.QDoubleValidator(0.0,300.0,1,notation=QtGui.QDoubleValidator.StandardNotation))
+        # self.ui.cmbWeight.setValidator(QtGui.QDoubleValidator(0.0,300.0,1,notation=QtGui.QDoubleValidator.StandardNotation))
 
         self.ui.btnSubmit.clicked.connect(self.recinsert)
 
 
     def recinsert(self):
-        r = self.model.record()
-        r.setValue("ftime", self.ui.dateTimeEdit.dateTime().toString("yyyy-MM-dd hh:mm"))
-        r.setValue("weight", int(self.ui.cmbWeight.currentText()))
-        r.setValue("bmi", int(self.ui.cmbBmi.currentText()))
-        self.model.insertRecord(-1, r)
-        self.model.submit()
+        try:
+            r = self.model.record()
+            r.setValue("ftime", self.ui.dateTimeEdit.dateTime().toString("yyyy-MM-dd hh:mm"))
+            r.setValue("weight", float(self.ui.spnWeight.text()))
+            r.setValue("bmi", float(self.ui.spnBmi.text()))
+            r.setValue("body_fat", float(self.ui.spnBodyfat.text()))
+            r.setValue("fat_free_body_weight", float(self.ui.spnFatfree.text()))
+            r.setValue("subcutaneous_fat", float(self.ui.spnSubfat.text()))
+            r.setValue("visceral_fat", float(self.ui.spnViscfat.text()))
+            r.setValue("body_water", float(self.ui.spnWater.text()))
+            r.setValue("muscle_mass", float(self.ui.spnMuscle.text()))
+            r.setValue("skeletal_muscles", float(self.ui.spnSkelmuscle.text()))
+            r.setValue("bone_mass", float(self.ui.spnBonemass.text()))
+            r.setValue("protein", float(self.ui.spnProtein.text()))
+            r.setValue("bmr", float(self.ui.spnBmr.text()))
+            r.setValue("metabolic_age", float(self.ui.spnAge.text()))
+            self.model.insertRecord(-1, r)
+            self.model.submit()
+
+            self.submit_OK = self.model.select()
+            self.message(f"record added: {self.submit_OK}  errors:  {self.model.lastError().text()}")
+        except:
+            print(f"please check inputs are in range {self.model.lastError().text()} ")
+
+
+    def message(self, s):
+        self.ui.txtMsgbox.appendPlainText(s)
+
+
+    def exitfunc(self):
+        self.db.close()
+        self.model.database().close()
+        if self.db.isOpen():
+            self.db.removeDatabase(self.conn_name)
         ic(self.model.lastError().text())
-        self.submit_OK = self.model.select()
-        ic(self.model.submit())
+        self.close()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
